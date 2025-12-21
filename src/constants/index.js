732ddfb642e26e41f7be97bc399840ab5dcfab97
marshallrussell <marshallrussell.x@gmail.com>
2025-12-20 20:30:23 -0600

    new resume file

--- src/constants/index.js
+++ src/constants/index.js
@@ -82,7 +82,7 @@ import {
 
 import { DiCss3, DiJava, DiMsqlServer, DiRuby } from "react-icons/di";
 
-export const resumeLink = "https://h007.weebly.com/uploads/1/4/6/0/146046514/marshallrussell-onlineresume.pdf";
+export const resumeLink = "https://h007.weebly.com/uploads/1/4/6/0/146046514/claremont_chicago_resume.pdf";
 export const repoLink = "https://github.com/marshallrussell/personal-portfolio";
 
 export const callToAction = "https://www.linkedin.com/in/yesmarshall";
